cccc0f0858df5676cd867fdbb29532bf76afa176
tobias2910 <33369379+tobias2910@users.noreply.github.com>
2022-10-10 23:49:33 +0200

    Connected to Blog API in the welcome and projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,38 +1,72 @@
 import React from 'react';
 
-import type { NextPage } from 'next';
-import { SiReact } from 'react-icons/si';
+import type { InferGetStaticPropsType } from 'next';
 
 import ProjectCard from '@components/common/ProjectCard';
+import { Token } from '../../typings/token';
+import { Project } from '../../typings/project';
 
-const Projects: NextPage = () => (
-  <div className="w-10/12 text-center m-auto">
-    <span className="text-xl">
-      Currently in development. In the future, you will find here an overview of
-      the projects I have finished so far.
-    </span>
-    <div className="grid grid-flow-row">
+export const getStaticProps = async () => {
+  const authRes = await fetch(
+    `${process.env.BLOG_RESTAPI_URL}/api/v1/auth/login`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: process.env.API_USERNAME,
+        password: process.env.API_PASSWORD,
+      }),
+    }
+  );
+  const token: Token = await authRes.json();
+
+  if (!authRes.ok) {
+    throw new Error(
+      `Failed to fetch auth token. Error code ${authRes.statusText}`
+    );
+  }
+
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token.access_token.token}`,
+  });
+
+  const projectsRes = await fetch(
+    `${process.env.BLOG_RESTAPI_URL}/api/v1/projects/`,
+    { headers }
+  );
+
+  if (!projectsRes.ok) {
+    throw new Error(
+      `Failed to fetch new skills. Error code ${projectsRes.statusText}`
+    );
+  }
+
+  const projects: Project[] = await projectsRes.json();
+  return {
+    props: {
+      projects,
+    },
+    revalidate: 6000,
+  };
+};
+
+const Projects = ({
+  projects,
+}: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <div className="grid grid-col-1 md:grid-cols-2 grid-flow-row gap-2">
+    {projects.map((project) => (
       <ProjectCard
-        title="My super asset project"
-        description="This is my super super asset project that I really hate since it isnt that good. Unfortunantely. But I will do it better"
-        imageUrl="/avatar.jpeg"
-        projectUrl="https://asset-monitoring.de"
-        tags={[
-          { icon: SiReact, text: 'React' },
-          { icon: SiReact, text: 'React' },
-          { icon: SiReact, text: 'TailwindCSS' },
-          { icon: SiReact, text: 'React' },
-          { icon: SiReact, text: 'React' },
-        ]}
-      />{' '}
-      <ProjectCard
-        title="My super asset project"
-        description="This is my description. is is my description. This is my description"
-        imageUrl="/avatar.jpeg"
-        projectUrl="https://asset-monitoring.de"
-        tags={[{ icon: SiReact, text: 'React' }]}
+        title={project.title}
+        description={project.description}
+        imageUrl={project.image_url}
+        projectUrl={project.project_url}
+        tags={project.tags}
+        key={project.id}
       />
-    </div>
+    ))}
   </div>
 );
 
