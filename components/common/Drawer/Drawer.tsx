99bd23ddd12584ed1e0794166d61e6224c0184c5
tobias2910 <33369379+tobias2910@users.noreply.github.com>
2022-11-01 23:47:59 +0100

    Added the exit animation for the drawer

--- components/common/Drawer/Drawer.tsx
+++ components/common/Drawer/Drawer.tsx
@@ -1,25 +1,32 @@
-import React, { FC, forwardRef, Ref } from 'react'
-import NavigationItems from '../NavigationItems'
+import React, { FC, useRef } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import NavigationItems from '../NavigationItems';
 
 interface DrawerProps {
-  isOpen: boolean
-  navItems: string[]
-  ref?: Ref<HTMLDivElement>
+  isOpen: boolean;
+  navItems: string[];
 }
 
-const Drawer: FC<DrawerProps> = forwardRef(
-  ({ isOpen, navItems }, forwardedRef) => (
+const Drawer: FC<DrawerProps> = ({ isOpen, navItems }) => {
+  const nodeRef = useRef(null);
+  return (
     <div className="w-40 fixed flex md:hidden z-[1]">
-      {isOpen ? (
-        <div
-          ref={forwardedRef}
-          className="h-screen backdrop-blur-md animate-fadeIn-left"
-        >
+      <CSSTransition
+        nodeRef={nodeRef}
+        classNames={{
+          enter: 'animate-fadeIn-left',
+          exit: 'animate-fadeOut-left',
+        }}
+        unmountOnExit
+        in={isOpen}
+        timeout={200}
+      >
+        <div ref={nodeRef} className="h-screen backdrop-blur-md">
           <NavigationItems alignment="left" items={navItems} />
         </div>
-      ) : null}
+      </CSSTransition>
     </div>
-  )
-)
+  );
+};
 
-export default Drawer
+export default Drawer;
